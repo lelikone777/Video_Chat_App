3c5ab58c417b2478406949bba7a63f7febb1955f
Aleksei Kalinin <2257855@gmail.com>
2024-04-05 09:00:09 +0400

    hot fix

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -121,7 +121,7 @@ const MeetingTypeList = () => {
             <label className="text-base font-normal leading-[22.4px] text-sky-2">
               Select Date and Time
             </label>
-            {/*<ReactDatePicker*/}
+            {/* <ReactDatePicker */}
             {/*  selected={values.dateTime}*/}
             {/*  onChange={(date) => setValues({ ...values, dateTime: date! })}*/}
             {/*  showTimeSelect*/}
@@ -130,7 +130,7 @@ const MeetingTypeList = () => {
             {/*  timeCaption="time"*/}
             {/*  dateFormat="MMMM d, yyyy h:mm aa"*/}
             {/*  className="w-full rounded bg-dark-3 p-2 focus:outline-none"*/}
-            {/*/>*/}
+            {/* /> */}
           </div>
         </MeetingModal>
       ) : (
