6c811d52c09d5b2551686bc2044b43df55c6f03f
Aleksei Kalinin <2257855@gmail.com>
2024-04-07 11:23:31 +0400

    hotfix5

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,13 +1,13 @@
 import CallList from "@/components/CallList";
 
-const UpcomingPage = () => {
+const PreviousPage = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">Upcoming Meeting</h1>
+      <h1 className="text-3xl font-bold">Recordings</h1>
 
-      <CallList type="upcoming" />
+      <CallList type="recordings" />
     </section>
   );
 };
 
-export default UpcomingPage;
+export default PreviousPage;
